81f7f236d854db975890f658b3cda0c0bfdff8bf
Niels Gregers Johansen <niels@timeboxer.dk>
2024-11-09 09:44:51 +0100

    fix

--- .koksmat/web/components/form-access-point.tsx
+++ .koksmat/web/components/form-access-point.tsx
@@ -7,7 +7,7 @@ import { GenericTableEditor } from '@/app/koksmat/src/v.next/components'
 
 export default function TableEditor() {
   return (
-    <GenericTableEditor schema={databases.tools.table.accesspoint.schema} />
+    <GenericTableEditor schema={databases.tools.table.accesspoint.schema} tableName={databases.tools.table.accesspoint.tablename} />
   )
 }
 
